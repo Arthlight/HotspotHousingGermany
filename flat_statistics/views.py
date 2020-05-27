904f0026e619c1c0c9051d85384db0a660871c58
Arthlight <arthurpbh@gmail.com>
2020-05-27 12:18:49 +0200

    lots of clean code approvements

--- flat_statistics/views.py
+++ flat_statistics/views.py
@@ -20,7 +20,7 @@ def get_stats(request, *args, **kwargs):
     city = flat_maps_comp.compute_area_mean_prices_for(city)
 
     # sorted such that the areas are being displayed from the most expensive one down to the least expensive one
-    labels = sorted([key for key in city.mean_table], key=lambda key: city.mean_table[key][1], reverse=True)
+    labels = sorted([area for area in city.mean_table], key=lambda area: city.mean_table[area][1], reverse=True)
     items = sorted([city.mean_table[area][1] // city.mean_table[area][0] for area in labels], reverse=True)
     data = {
         'labels': labels,
